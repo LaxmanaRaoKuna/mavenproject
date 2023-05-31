9e8e3ba3335bfe07437b6161e67402c6d1eaefac
LaxmanaRaoKuna <laxmanaraokuna@gamil.com>
2023-05-31 10:00:37 +0530

    maven sample project

---
+++ src/test/java/com/learning/MavenDemo/Maven/Demo/MavenDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.learning.MavenDemo.Maven.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenDemoApplicationTests {
+
+	public static void main(String args[]) {
+		
+		System.out.println("good morning");
+		}
+
+}
